2d0243bc26397416b6d8690ddd2aeea1475fe41c
정연호 <hhyy8892@gmail.com>
2022-07-11 09:35:17 +0900

    unique

--- App.tsx
+++ App.tsx
@@ -1,10 +1,4 @@
-import React, {
-  MutableRefObject,
-  Suspense,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, {Suspense, useRef, useState} from 'react';
 import {
   Header,
   Root,
@@ -27,144 +21,17 @@ import {
   ModalButton,
   ModalButtonText,
 } from './components';
-import {Alert, Modal, Animated, View, Easing, ViewStyle} from 'react-native';
+import {Alert, Modal} from 'react-native';
 import {RecoilRoot, useRecoilState} from 'recoil';
 import {todo_list} from './recoilState';
-import {
-  GestureHandlerRootView,
-  PanGestureHandler,
-} from 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import SlideView, {ISetXRef} from './components/SlideView';
 
 interface ITodos {
   value: string;
   complete: boolean;
+  date: number;
 }
-
-interface ISlideView {
-  MainView: JSX.Element;
-  left?: number;
-  right?: number;
-  rightBreak?: number;
-  leftBreak?: number;
-  LeftView?: JSX.Element;
-  RightView?: JSX.Element;
-  rootStyle?: ViewStyle;
-  animationDuration?: number;
-  setXRef?: MutableRefObject<{
-    setXValue?: (value: number) => void;
-    animateXValue?: (value: number) => void;
-  }>;
-}
-
-const SlideView = ({
-  MainView,
-  left,
-  right,
-  rightBreak,
-  leftBreak,
-  LeftView,
-  RightView,
-  rootStyle,
-  animationDuration,
-  setXRef,
-}: ISlideView) => {
-  const AnimationX = useRef(new Animated.Value(0)).current;
-  const X = useRef(0);
-
-  useEffect(() => {
-    if (setXRef) {
-      setXRef.current = {
-        setXValue: value => {
-          X.current = value;
-          AnimationX.setValue(value);
-        },
-        animateXValue: value => {
-          X.current = value;
-
-          Animated.timing(AnimationX, {
-            useNativeDriver: true,
-            toValue: X.current,
-            duration: animationDuration ?? 200,
-            easing: Easing.ease,
-          }).start();
-        },
-      };
-    }
-  }, []);
-
-  return (
-    <>
-      <PanGestureHandler
-        onGestureEvent={({nativeEvent}) => {
-          AnimationX.setValue(
-            X.current !== 0
-              ? X.current + nativeEvent.translationX
-              : nativeEvent.translationX,
-          );
-        }}
-        onHandlerStateChange={({nativeEvent}) => {
-          if (nativeEvent.state === 5) {
-            if (nativeEvent.translationX >= 0 && leftBreak) {
-              X.current = nativeEvent.translationX < leftBreak ? leftBreak : 0;
-            } else if (rightBreak) {
-              X.current =
-                nativeEvent.translationX < rightBreak * -1
-                  ? rightBreak * -1
-                  : 0;
-            }
-
-            Animated.timing(AnimationX, {
-              useNativeDriver: true,
-              toValue: X.current,
-              duration: animationDuration ?? 200,
-              easing: Easing.ease,
-            }).start();
-          }
-        }}>
-        <View
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            ...(rootStyle ?? {}),
-          }}>
-          {LeftView && (
-            <View
-              style={{
-                position: 'absolute',
-                left: left ?? 0,
-                zIndex: -1,
-              }}>
-              {LeftView}
-            </View>
-          )}
-          {RightView ?? (
-            <View
-              style={{
-                position: 'absolute',
-                right: right ?? 0,
-                zIndex: -1,
-              }}>
-              {RightView}
-            </View>
-          )}
-          <Animated.View
-            style={{
-              transform: [
-                {
-                  translateX: AnimationX,
-                },
-                {
-                  translateY: 0,
-                },
-              ],
-            }}>
-            {MainView}
-          </Animated.View>
-        </View>
-      </PanGestureHandler>
-    </>
-  );
-};
 
 const TodoTouch = ({
   todo,
@@ -177,9 +44,10 @@ const TodoTouch = ({
   item: ITodos;
   index: number;
 }) => {
-  const setXRef = useRef({
+  const setXRef = useRef<ISetXRef>({
     setXValue: (_: number) => {},
   });
+  const slideRef = useRef(0);
 
   return (
     <>
@@ -188,7 +56,7 @@ const TodoTouch = ({
           <TodoView
             activeOpacity={1}
             onPress={() => {
-              setXRef.current.setXValue(0);
+              setXRef.current.animateXValue && setXRef.current.animateXValue(0);
               setTodo([
                 {
                   ...item,
@@ -213,7 +81,7 @@ const TodoTouch = ({
         RightView={
           <DelView
             onPress={() => {
-              setXRef.current.setXValue(0);
+              setXRef.current.animateXValue && setXRef.current.animateXValue(0);
               setTodo(
                 [
                   ...todo.filter(s => !s.complete),
@@ -224,6 +92,7 @@ const TodoTouch = ({
             <DelText>삭제</DelText>
           </DelView>
         }
+        slide={slideRef}
       />
       <Border />
     </>
@@ -261,6 +130,7 @@ const Application = () => {
                       {
                         value: input,
                         complete: false,
+                        date: Date.now(),
                       },
                       ...todo,
                     ]);
@@ -302,6 +172,7 @@ const Application = () => {
               setTodo={setTodo}
               item={item}
               index={index}
+              key={item.value}
             />
           );
         }}
---
+++ components/SlideView.tsx
@@ -0,0 +1,136 @@
+import {Animated, Easing, View, ViewStyle} from 'react-native';
+import React, {MutableRefObject, useEffect, useRef} from 'react';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+
+export interface ISetXRef {
+  setXValue?: (value: number) => void;
+  animateXValue?: (value: number) => void;
+}
+
+export interface ISlideView {
+  MainView: JSX.Element;
+  slide: MutableRefObject<number>;
+  left?: number;
+  right?: number;
+  rightBreak?: number;
+  leftBreak?: number;
+  LeftView?: JSX.Element;
+  RightView?: JSX.Element;
+  rootStyle?: ViewStyle;
+  animationDuration?: number;
+  setXRef?: MutableRefObject<ISetXRef>;
+}
+
+const SlideView = ({
+  MainView,
+  slide,
+  left,
+  right,
+  rightBreak,
+  leftBreak,
+  LeftView,
+  RightView,
+  rootStyle,
+  animationDuration,
+  setXRef,
+}: ISlideView) => {
+  const AnimationX = useRef(new Animated.Value(slide.current)).current;
+
+  useEffect(() => {
+    if (setXRef) {
+      setXRef.current = {
+        setXValue: value => {
+          slide.current = value;
+
+          AnimationX.setValue(value);
+        },
+        animateXValue: value => {
+          slide.current = value;
+
+          Animated.timing(AnimationX, {
+            useNativeDriver: true,
+            toValue: slide.current,
+            duration: animationDuration ?? 200,
+            easing: Easing.ease,
+          }).start();
+        },
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <PanGestureHandler
+        onGestureEvent={({nativeEvent}) => {
+          AnimationX.setValue(
+            slide.current !== 0
+              ? slide.current + nativeEvent.translationX
+              : nativeEvent.translationX,
+          );
+        }}
+        onHandlerStateChange={({nativeEvent}) => {
+          if (nativeEvent.state === 5) {
+            if (nativeEvent.translationX >= 0 && leftBreak) {
+              slide.current =
+                nativeEvent.translationX < leftBreak ? leftBreak : 0;
+            } else if (rightBreak) {
+              slide.current =
+                nativeEvent.translationX < rightBreak * -1
+                  ? rightBreak * -1
+                  : 0;
+            }
+
+            Animated.timing(AnimationX, {
+              useNativeDriver: true,
+              toValue: slide.current,
+              duration: animationDuration ?? 200,
+              easing: Easing.ease,
+            }).start();
+          }
+        }}>
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            ...(rootStyle ?? {}),
+          }}>
+          {LeftView && (
+            <View
+              style={{
+                position: 'absolute',
+                left: left ?? 0,
+                zIndex: -1,
+              }}>
+              {LeftView}
+            </View>
+          )}
+          {RightView ?? (
+            <View
+              style={{
+                position: 'absolute',
+                right: right ?? 0,
+                zIndex: -1,
+              }}>
+              {RightView}
+            </View>
+          )}
+          <Animated.View
+            style={{
+              transform: [
+                {
+                  translateX: AnimationX,
+                },
+                {
+                  translateY: 0,
+                },
+              ],
+            }}>
+            {MainView}
+          </Animated.View>
+        </View>
+      </PanGestureHandler>
+    </>
+  );
+};
+
+export default SlideView;
